7ae59e38e3365fd1c96ed9f66d94ffacb507d92c
Cleo C Abram <cleocabram@Cleos-MacBook-Pro.local>
2016-03-27 11:52:46 -0400

    here's hoping this works

--- story.rb
+++ story.rb
@@ -2,14 +2,20 @@ require 'pry'
 
 #write a method that accepts two parameters: title and rating
   #the method should print the story title and the rating out of ten
-
+def display_story(title, rating)
+  puts("The story title is #{title} and its rating is #{rating}.")
+end
 
 #show the user a message asking them for a title
+puts("What is the story title?")
 
 #store the input in a variable
+user_title = gets.strip
 
 #show the user a message asking for their rating of the movie out of ten
+puts("What is the story's rating?")
 
 # store the input in a variable
+user_rating = gets.strip
 
 display_story(user_title, user_rating)
